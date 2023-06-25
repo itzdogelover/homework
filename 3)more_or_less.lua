072c157189ef1c09c1ba57b1e5cca61a2cbbfffe
dachi <114247817+itzdogelover@users.noreply.github.com>
2023-06-25 13:09:31 +0400

    Add files via upload

---
+++ 3)more_or_less.lua
@@ -0,0 +1,11 @@
+math.randomseed(os.time())
+
+number = math.random(1,20)
+
+if number > 10 then
+print(true)
+elseif number < 10 then
+print(false)
+elseif number == 10 then
+print('equals to ten')
+end
